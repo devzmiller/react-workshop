480c677ff1fa6ab1d9494bb57ed99a9c77e8d114
Devin Miller & Eliza Brine <pair+devzmiller.ebrine@devbootcamp.com>
2017-10-23 15:54:46 -0700

    Implement timer loading page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Page from './components/Page'
+import Loading from './components/Loading'
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      view: "loading"
+    }
+    this.loadPage = this.loadPage.bind(this)
+  }
+  loadPage() {
+    this.setState({view: 'page'})
+  }
+  componentDidMount() {
+    window.setTimeout(this.loadPage, 3000)
+  }
   render() {
-    return (
-    <Page />
-    );
+    if (this.state.view === 'loading') {
+      return (
+        <Loading />
+      )
+    } else {
+      return (
+        <Page />
+    )}
   }
 }
 
